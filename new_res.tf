af1af3c520483bf1bef80b1acdeabbf8004f986d
kamal1821986 <134919014+kamal1821986@users.noreply.github.com>
2023-06-02 12:29:04 +0530

    Create new_res.tf

---
+++ new_res.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+
+resource "aws_iam_user" "kamal" {
+  name = "kamal"  
+  path = "/system/"  
+  tags = {
+    tag-key = "kamal user"
+  }
+}
+
+resource "aws_instance" "kkec2" {
+ ami = "ami-0715c1897453cabd1"
+ instance_type = "t2.micro"
+}
+
+
+resource "aws_s3_bucket" "kkbucket" {
+  bucket = "my-tf-test-kk-bucket"
+  tags = {
+    Name = "kk bucket"
+    Environment = "Dev"
+    }
+   }
+   
+ 
+resource "aws_security_group" "kkec2-sg" {
+    name = "kkmyec2-sg"
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
+
